6524a70239a8441475443d69588b27c90315e97b
dltjsam <dltjsam@gmail.com>
2023-01-27 10:33:45 +0900

    react #07

---
+++ 예제/my-app/src/ex07/Accommodate.jsx
@@ -0,0 +1,10 @@
+import React, {useState, useEffect } from "react";
+import useCounter from "./userCounter";
+
+const MAX_CAPACITY = 10;
+
+function Accommodate(props) {
+
+}
+
+export default Accommodate;
