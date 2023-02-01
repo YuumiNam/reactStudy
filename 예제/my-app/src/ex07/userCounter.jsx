fb93764120b345f0cd55d816f3e3508e9e020aff
dltjsam <dltjsam@gmail.com>
2023-02-01 16:54:05 +0900

    수정

--- 예제/my-app/src/ex07/userCounter.jsx
+++ 예제/my-app/src/ex07/userCounter.jsx
@@ -5,3 +5,31 @@ function useCounter() {
 }
 
 export default useCounter;
+
+
+
+import {useState } from "react";
+
+function ParentComponent(props) {
+    const [count, setCount] = useState(0);
+
+    // const handClick = (event) => {
+    //     // 클릭 이벤트 처리
+    // };
+
+    const handClick = useCallback((event) => {
+        // 클릭 이벤트 처리
+    }, []);
+
+    return (
+        <div>
+            <button onClick={() => {
+                setCount(count + 1);
+            }}>
+                {count }
+            </button>
+
+            <ChildComponent handClick={handClick } />
+        </div>
+    );
+}
